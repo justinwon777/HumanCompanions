7a50524d05f0b7c222db3b8f51a3937b40df2902
justinwon777 <justinwon777@gmail.com>
2022-01-25 20:03:29 -0600

    Adjusted follow speed so it can catch up

--- forge 1.16.5/src/main/java/com/github/justinwon777/humancompanions/entity/AbstractHumanCompanionEntity.java
+++ forge 1.16.5/src/main/java/com/github/justinwon777/humancompanions/entity/AbstractHumanCompanionEntity.java
@@ -54,7 +54,7 @@ public class AbstractHumanCompanionEntity extends TameableEntity {
         this.goalSelector.addGoal(0, new SwimGoal(this));
         this.goalSelector.addGoal(1, new SitGoal(this));
         this.goalSelector.addGoal(2, new AvoidCreeperGoal(this, CreeperEntity.class, 10.0F, 1.5D, 1.5D));
-        this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.addGoal(3, new FollowOwnerGoal(this, 1.3D, 8.0F, 2.0F, false));
         this.goalSelector.addGoal(4, new WaterAvoidingRandomWalkingGoal(this, 1.0D));
         this.goalSelector.addGoal(5, new LookAtGoal(this, PlayerEntity.class, 6.0F));
         this.goalSelector.addGoal(6, new LookRandomlyGoal(this));

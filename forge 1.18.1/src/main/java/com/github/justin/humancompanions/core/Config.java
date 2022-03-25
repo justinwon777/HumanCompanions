28569c59c5d95cef7608385b6dc688703b45d5d0
justinwon777 <justinwon777@gmail.com>
2022-03-25 02:12:36 -0500

    added config files

---
+++ forge 1.18.1/src/main/java/com/github/justin/humancompanions/core/Config.java
@@ -0,0 +1,23 @@
+package com.github.justin.humancompanions.core;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
+
+public class Config {
+
+    public static ForgeConfigSpec.IntValue AVERAGE_HOUSE_SEPARATION;
+
+    public static void register() {
+        registerCommonConfig();
+    }
+
+    private static void registerCommonConfig() {
+        ForgeConfigSpec.Builder COMMON_BUILDER = new ForgeConfigSpec.Builder();
+        COMMON_BUILDER.comment("Settings for world gen").push("world gen");
+        AVERAGE_HOUSE_SEPARATION = COMMON_BUILDER
+                .comment("Average chunk separation between companion houses")
+                .defineInRange("averageHouseSeparation", 20, 11, Integer.MAX_VALUE);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, COMMON_BUILDER.build());
+    }
+}
